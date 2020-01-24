f87d9d31491d6e7be28d7c60e80759e6a9496a70
Anton <antonr1@bk.ru>
2020-01-24 17:25:27 +0300

    add second main task

--- script/script.js
+++ script/script.js
@@ -1,9 +1,16 @@
-let money; 
-let income; 
-let addExpenses; 
-let deposit; 
-let mission; 
-let period;
+let money = 90000; 
+let income = 'фриланс'; 
+let addExpenses = 'Квартплата, кредит, еда'; 
+let deposit = true; 
+let mission = 500000; 
+let period = 12;
+let budgetDay = money / 30;
 
-alert('Hello World');
-console.log('Мир, дружба, жвачка!');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+console.log('budgetDay: ', budgetDay);
