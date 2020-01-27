e1c8759137f4061ac191f2db1308e749a9e45292
Anton <antonr1@bk.ru>
2020-01-27 18:12:45 +0300

    add third main task

--- script/script.js
+++ script/script.js
@@ -1,8 +1,11 @@
-let money = 90000; 
+'use strict';
+
+let money = prompt('Ваш месячный доход?'); // узнаем месячный доход и записываем в переменную money
 let income = 'фриланс'; 
-let addExpenses = 'Квартплата, кредит, еда'; 
-let deposit = true; 
-let mission = 500000; 
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'); /* спрашиваем о 
+расходах и заносим в переменную addExpenses */
+let deposit = confirm('Есть ли у вас депозит в банке?'); // узнаем о наличии депозита в банке и получаем true или false
+let mission = 1000000; 
 let period = 12;
 let budgetDay = money / 30;
 
@@ -14,3 +17,64 @@ console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
 console.log(addExpenses.toLowerCase().split(', '));
 console.log('budgetDay: ', budgetDay);
+
+console.log('money: ', money);
+console.log('addExpenses: ', addExpenses);
+console.log('deposit: ', deposit);
+
+// Создадим функцию для создания массива с указанием Статьи расходов и её стоимости, где k - количество статей
+const recordExpenses = function (k) {
+    let obj = {};
+
+    for (let i = 1; i <= k; i++) {
+        let expenses = prompt('Введите обязательную статью расходов?');
+        let amount = prompt('Во сколько это обойдется??');
+        obj[expenses] = amount;
+    }
+    console.log(obj);
+    return obj;
+};
+
+let expenseItems = recordExpenses(2); // записываем объект со значениями расходов в переменную expenses
+let expenses = Object.values(expenseItems); // получим значения "ключей" у объекта
+
+// Создадим функцию, которая суммирует все расходы
+const doAddition = function (array) {
+    let result = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        result += +array[i];
+    }
+    return result;
+
+};
+
+let totalExpenses = doAddition(expenses); //создадим переменную для записи всех расходов
+
+// Вычисляем бюджет на месяц = месячный доход - обязательные траты
+let budgetMonth = money - totalExpenses;
+console.log('Бюджет на месяц: ', budgetMonth);
+
+//Вычисляем количество месяцев для достижения цели
+let months = Math.ceil(mission / budgetMonth);
+console.log('Цель будет достигнута за ' + months + ' месяцев');
+
+// Пересчитаем бюджет на день
+budgetDay = Math.floor(budgetMonth / 30);
+console.log('Бюджет на день: ', budgetDay);
+
+//Записываем конструкцию условий для определения уровня дохода по шкале GloAcademy =)
+switch (!!budgetDay) {
+    case (budgetDay > 1200):
+        console.log('У вас высокий уровень дохода1');
+        break;
+    case ((budgetDay >= 600) && (budgetDay <= 1200)):
+        console.log('У вас средний уровень дохода');
+        break;
+    case ((budgetDay > 0) && (budgetDay < 600)):
+        console.log('К сожалению у вас уровень дохода ниже среднего');
+        break;
+    case (budgetDay <= 0):
+        console.log('Что то пошло не так');
+        break;
+}
