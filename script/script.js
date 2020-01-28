85957a17b5208d2c0a69f1794de993c2604f96fc
Anton <antonr1@bk.ru>
2020-01-29 00:11:13 +0300

    fix fourth task

--- script/script.js
+++ script/script.js
@@ -1,14 +1,12 @@
 'use strict';
 
-let money = prompt('Ваш месячный доход?'); // узнаем месячный доход и записываем в переменную money
-let income = 'фриланс';
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+const money = +prompt('Ваш месячный доход?'); // узнаем месячный доход и записываем в переменную money
+const income = 'фриланс';
+const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
 /* спрашиваем о 
 расходах и заносим в переменную addExpenses */
-let deposit = confirm('Есть ли у вас депозит в банке?'); // узнаем о наличии депозита в банке и получаем true или false
-let mission = 1000000;
-let period = 12;
-let budgetDay;
+const deposit = confirm('Есть ли у вас депозит в банке?'); // узнаем о наличии депозита в банке и получаем true или false
+const mission = 1000000;
 
 // Создаём функцию для определия типа данных
 const showTypeOf = function (data) {
@@ -19,7 +17,7 @@ showTypeOf(income);
 showTypeOf(deposit);
 
 //Переведем строку расходов в массив
-let arrExpenses = addExpenses.toLowerCase().split(',');
+const arrExpenses = addExpenses.toLowerCase().split(',');
 
 for (let i = 0; i < arrExpenses.length; i++) {
     arrExpenses[i] = arrExpenses[i].trim();
@@ -34,15 +32,13 @@ const recordExpenses = function (k) {
 
     for (let i = 1; i <= k; i++) {
         let expenses = prompt('Введите обязательную статью расходов?');
-        let amount = prompt('Во сколько это обойдется??');
-        obj[expenses] = amount;
+        obj[expenses] = prompt('Во сколько это обойдется??');
     }
-    console.log(obj);
     return obj;
 };
 
 // получим значения "ключей" у объекта, в котором хранятся значения расходов
-let expenses = Object.values(recordExpenses(2)); 
+const expenses = Object.values(recordExpenses(2)); 
 
 // Создадим функцию, которая суммирует всех обязательных расходов
 const getExpensesMonth = function (array) {
@@ -62,36 +58,35 @@ const getAccumulatedMonth = function(income, costs) {
 };
 
 // Объявляем переменную и присваиваем ей результат функции
-let accumulatedMonth  = getAccumulatedMonth(money, getExpensesMonth(expenses));
+const accumulatedMonth  = getAccumulatedMonth(money, getExpensesMonth(expenses));
 
 //Объявляем функцию, подсчитывающая количество месяцев за который достигнем результат
 const getTargetMonth = function() {
-    return Math.ceil(mission / accumulatedMonth);
-};
-
-//Выведем срок достижения цели
-console.log('Цель будет достигнута через ' + getTargetMonth() + ' месяцев');
-
-// Пересчитаем бюджет на день
-budgetDay = Math.floor(accumulatedMonth / 30);
-console.log('Бюджет на день: ', budgetDay);
-
-//Записываем конструкцию условий для определения уровня дохода по шкале GloAcademy =)
-let getStatusIncome = function () {
-    switch (!!budgetDay) {
-        case (budgetDay > 1200):
-            console.log('У вас высокий уровень дохода');
-            break;
-        case ((budgetDay >= 600) && (budgetDay <= 1200)):
-            console.log('У вас средний уровень дохода');
-            break;
-        case ((budgetDay > 0) && (budgetDay < 600)):
-            console.log('К сожалению у вас уровень дохода ниже среднего');
-            break;
-        case (budgetDay <= 0):
-            console.log('Что то пошло не так');
-            break;
+    if (accumulatedMonth === 0 || accumulatedMonth < 0) {
+        return 'цель не будет достигнута пока не увеличите бюджет!';
+    } else {
+        return Math.ceil(mission / accumulatedMonth);
     }
 };
 
-getStatusIncome();
+//Выведем срок достижения цели
+console.log('Срок достижения цели(в месяцаx): ' + getTargetMonth());
+
+// Пересчитаем бюджет на день
+const budgetDay = Math.floor(accumulatedMonth / 30);
+console.log('Бюджет на день: ', budgetDay);
+
+//Записываем конструкцию условий для определения уровня дохода по шкале GloAcademy =)
+const getStatusIncome = function () {
+    if (budgetDay > 1200) {
+        return 'У вас высокий уровень дохода';
+    } else if ((budgetDay >= 600) && (budgetDay <= 1200)) {
+        return 'У вас средний уровень дохода';
+    } else if ((budgetDay > 0) && (budgetDay < 600)) {
+        return 'К сожалению у вас уровень дохода ниже среднего';
+    } else {
+        return 'Что то пошло не так';
+    }
+};
+
+console.log(getStatusIncome());
