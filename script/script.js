485d71913d0392fd3e57f24d4d108082809937ba
Anton <antonr1@bk.ru>
2020-01-29 12:36:51 +0300

    fix condition in fuction

--- script/script.js
+++ script/script.js
@@ -1,11 +1,14 @@
 'use strict';
-let str = '               Привет, как дела? У меня всё хорошо, надеюсь и у вас тоже всё здорово!'           ;
+let str = '               Привет, как дела? У меня всё хорошо, надеюсь и у вас тоже всё здорово!';
+let str2 = 'привет';
 let num = 123;
 let arr = [];
 
-const a = function(data) {
+const a = function (data) {
     if (typeof data !== 'string') {
         return 'Введён не строчный тип данных';
+    } else if (data.length < 30) {
+        return data;
     } else {
         return data.trim().substr(0, 30) + '...';
     }
@@ -14,6 +17,4 @@ const a = function(data) {
 console.log(a(str));
 console.log(a(num));
 console.log(a(arr));
-
-
-
+console.log(a(str2));
