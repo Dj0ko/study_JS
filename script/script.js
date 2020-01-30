0c14319e3ce5ec600283e1489666485f33f92d54
Anton <antonr1@bk.ru>
2020-01-31 01:04:48 +0300

    add sixth extra task

--- script/script.js
+++ script/script.js
@@ -1,39 +1,61 @@
 'use strict';
 //функция для получения случайного числа в заданном интервале
-const getRandom = function(min, max) {
+function getRandom(min, max) {
     return Math.round(Math.random() * (max - min) + min);
-};
+}
+
+//Создаем функцию , которая при ошибочном вводе будет уменьшать количество попыток
+function changeAttempts(n) {
+    n--;
+    if (n > 0) {
+        return n;
+    } else {
+        // если попытки закончились - предлагаем сыграть еще раз
+        let message = confirm('Попытки закончились, хотите сыграть еще?');
+        if (message) {
+            startGame();
+        } else {
+            alert('До скорых встреч!');
+        }
+    }
+}
 
 //функция для угадывания числа
 function startGame() {
-let randomValue = getRandom(1, 100); //Задаем случайное число в диапозоне от 1 до 100
+    let randomValue = getRandom(1, 100); //Задаем случайное число в диапозоне от 1 до 100
+    let attempts = 1; //задаём начальное количество попыток
 
-function askQuestion() {
-    const setValue = +prompt('Угадай число от 1 до 100.');
-    // Сравниваем введённое значение с загаданным 
-    if (setValue > randomValue) {
-        alert('Загаданное число меньше');
-        askQuestion();
-    } else if (setValue < randomValue && setValue > 0) {
-        alert('Загаданное число больше');
-        askQuestion();
-    } else if (isNaN(setValue)) { //проверяем чтоб введено было число
-        alert('Введи число');
-        askQuestion();
-    } else if (setValue === 0) { //выводим сообщение об окончании игры, если нажали кнопку "Отмена"
-        alert('Game Over');
-    } else if (setValue === randomValue) {
-        // Если угадали загаданное значение, то предлагаем сыграть еще или выйти из программы
-        let victoriusMessage = confirm('Победа! Хотите сыграть снова?');
-        if (victoriusMessage) {
-            randomValue = getRandom(1, 100);
+
+    function askQuestion() {
+        // debugger;
+        const setValue = +prompt('Угадай число от 1 до 100!');
+        attempts = changeAttempts(attempts);
+        // Сравниваем введённое значение с загаданным 
+        if (setValue > randomValue) {
+            // changeAttempts(attempts);
+            alert('Загаданное число меньше, осталось попыток: ' + attempts);
             askQuestion();
-        } else {
-            alert('До скорых встреч');
+        } else if (setValue < randomValue && setValue > 0) {
+            alert('Загаданное число больше, осталось попыток: ' + attempts);
+            askQuestion();
+        } else if (isNaN(setValue)) { //проверяем чтоб введено было число
+            alert('Введи число');
+            askQuestion();
+        } else if (setValue === 0) { //выводим сообщение об окончании игры, если нажали кнопку "Отмена"
+            alert('Game Over');   
+        } else if (setValue === randomValue) {
+            // Если угадали загаданное значение, то предлагаем сыграть еще или выйти из программы
+            let victoriusMessage = confirm('Победа! Хотите сыграть снова?');
+            if (victoriusMessage) {
+                randomValue = getRandom(1, 100);
+                attempts = 10;
+                askQuestion();
+            } else {
+                alert('До скорых встреч');
+            }
         }
     }
-};
-return askQuestion();
+    return askQuestion();
 }
 
 startGame();
