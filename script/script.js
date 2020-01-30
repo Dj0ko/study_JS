48eb7e6b6301e1f1a0125252439cd3472c85e852
Anton <antonr1@bk.ru>
2020-01-30 16:13:07 +0300

    add fifth extra task another method

--- script/script.js
+++ script/script.js
@@ -13,19 +13,34 @@ for (let i = 0; i < arr.length; i++) {
 
 console.log(newarr);
 
-// Получаем простые числа от 1 до 100
-for (let i = 1; i <= 100; i++) {
-    var num = 0;
+// // Получаем простые числа от 1 до 100
+var arr1 = [];
 
-    for (let j = 1; j <= i; j++) {
-        if ((i % j) !== 0) {
-            continue;
-        } else {
-            num +=1 ;
-        } 
+for (var i = 2; i < 100; i++) {
+  arr1[i] = i;
+}
+
+var p = 2;
+
+do {
+  for (i = 2 * p; i < 100; i += p) {
+    arr1[i] = false;
+  }
+
+  for (i = p + 1; i < 100; i++) {
+    if (arr1[i]) {
+        break;
+    } else {
+        continue;
     }
+  }
+  p = i;
+} while (p * p < 100); 
 
-    if (num === 2) {
-        console.log('Простоe число: ' + i + '. Делители этого числа: 1 и ' + i);
+let newarr1 = [];
+for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i]) {
+        newarr1.push(arr1[i]);
+        console.log('Простоe число: ' + arr1[i] + '. Делители этого числа: 1 и ' + arr1[i]);
     }
 }
