c610b19cdbbbf2846303253fa3dac514ee80deef
Anton <antonr1@bk.ru>
2020-01-31 12:38:20 +0300

    fix fifth extra task

--- script/script.js
+++ script/script.js
@@ -14,33 +14,39 @@ for (let i = 0; i < arr.length; i++) {
 console.log(newarr);
 
 // // Получаем простые числа от 1 до 100
-var arr1 = [];
+const count = 20; //задаём количество чисел
 
-for (var i = 2; i < 100; i++) {
+//создаем массив и заполняем его значениями
+let arr1 = [];
+
+for (var i = 2; i <= count; i++) {
   arr1[i] = i;
 }
+console.log(arr1);
 
-var p = 2;
-
+let divider = 2; // задаём начальное значение делителя
+// перебираем возможные делители от 2 до sqrt(n)
 do {
-  for (i = 2 * p; i < 100; i += p) {
+  //всем числам , которые делятся на делитель даём значение false
+  for (i = 2 * divider; i <= count; i += divider) {
     arr1[i] = false;
   }
-
-  for (i = p + 1; i < 100; i++) {
+  console.log(arr1);
+  //берём следующее число , если оно оказывается без флага false , то принимаем его за делитель
+  for (i = divider + 1; i <= Math.sqrt(count); i++) {
     if (arr1[i]) {
         break;
     } else {
         continue;
     }
   }
-  p = i;
-} while (p * p < 100); 
+  divider = i;
+} while (divider < Math.sqrt(count)); /*проверка, что делитель не может быть больше 
+квадратного корня от максимально заданного числа - так получаем максимальный делитель*/
 
-let newarr1 = [];
+//выводим все простые числа
 for (let i = 0; i < arr1.length; i++) {
     if (arr1[i]) {
-        newarr1.push(arr1[i]);
         console.log('Простоe число: ' + arr1[i] + '. Делители этого числа: 1 и ' + arr1[i]);
     }
 }
